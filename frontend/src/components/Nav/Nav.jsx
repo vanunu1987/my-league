a2653df62a6ab6ab8d2748db8828682ea26dde55
IshayMaya <ssaymama@gmail.com>
2019-07-24 16:48:34 +0300

    nav

--- frontend/src/components/Nav/Nav.jsx
+++ frontend/src/components/Nav/Nav.jsx
@@ -1,10 +1,12 @@
 import React from 'react' 
 import {NavLink} from 'react-router-dom'
-import classes from './Nav.module.css' 
-const nav = (props) =>(
-<div className={classes.nav}>
-    <NavLink to="/">Home</NavLink> |
-    <NavLink to="/about">About</NavLink>
-</div>
+import classes from './Nav.module.css'
+
+const nav = props => (
+    <div className={classes.nav}>
+        <NavLink to="/">Home</NavLink>|
+        <NavLink to="/about">About</NavLink>
+    </div>
 )
+
 export default nav 
